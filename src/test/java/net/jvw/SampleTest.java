95764c623d856e4b1967b66e5213a03ebf868204
jvwilge <jeroen@jpoint.nl>
2019-05-29 09:31:04 +0200

    Initial commit

---
+++ src/test/java/net/jvw/SampleTest.java
@@ -0,0 +1,13 @@
+package net.jvw;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+  @DisplayName("test")
+  @Test
+  void test() {
+    new Sample();
+  }
+}
